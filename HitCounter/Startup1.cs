f5492669f5e3692ed45c461f9ece99faa5de60f9
IEUser <IEUser@MSEDGEWIN10>
2017-04-15 08:47:44 -0700

    Create a simple Hub

---
+++ HitCounter/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HitCounter.Startup1))]
+
+namespace HitCounter
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
